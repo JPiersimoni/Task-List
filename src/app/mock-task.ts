a3f935ae7d642e77177c554b90269aea2ed65be2
Jpiersimoni <nachopiersimoni@gmail.com>
2022-05-01 12:30:31 -0300

    segundo commit

---
+++ src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASK: Task[] = [
+    {
+        id: 1,
+        text: "terminar esta tarea angular",
+        day: 'Marzo 29 a las 17:00',
+        reminder: true
+    }, 
+    {
+        id: 1,
+        text: "comprar comida",
+        day: 'Marzo 27 a las 17:00',
+        reminder: true
+    }, 
+    {
+        id: 1,
+        text: "sacar gata",
+        day: 'Marzo 30 a las 17:00',
+        reminder: false
+    }, 
+    {
+        id: 1,
+        text: "sacar pelota",
+        day: 'Marzo 29 a las 15:00',
+        reminder: false
+    }, 
+]
